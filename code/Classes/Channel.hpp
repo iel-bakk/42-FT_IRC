530e3a5fa89221cef493f27c06f070f7f66681ea
ismail el bakkouchi <iel-bakk@e1r4p3.1337.ma>
2023-04-15 02:17:47 +0000

    added some norme

--- code/Classes/Channel.hpp
+++ code/Classes/Channel.hpp
@@ -15,18 +15,18 @@ private:
 public:
     Channel();
     ~Channel();
-    int parse_channel(std:: string channel, Channel& msg_channel);
-    std::string get_channel_name();
-    std::string get_channel_password();
-    void    add_user_to_list(std::string user_nick);
-    void    print_users_list();
-    std::vector <std::string> get_users_list();
-    void    empty_channel();
-    void    add_admin(std::string user_nick);
-    void    remove_user_from_channel_list(std::string username);
-    bool    is_admin(std::string username);
-    bool    check_join_request(std::string request);
-    bool    is_empty(std::string check);
+    int             parse_channel(std:: string channel, Channel& msg_channel);
+    std::string     get_channel_name();
+    std::string     get_channel_password();
+    void            add_user_to_list(std::string user_nick);
+    void            print_users_list();
+    std::vector<std::string> get_users_list();
+    void            empty_channel();
+    void            add_admin(std::string user_nick);
+    void            remove_user_from_channel_list(std::string username);
+    bool            is_admin(std::string username);
+    bool            check_join_request(std::string request);
+    bool            is_empty(std::string check);
 };
 
 #endif
